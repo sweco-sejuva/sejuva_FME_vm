c61346495c390fd23afd795e8d7859d42486d873
rjcragg <rjcragg@gmail.com>
2015-04-22 16:07:52 -0700

    Update FMEData copy to SSD for future flexibility

--- OnstartConfiguration.bat
+++ OnstartConfiguration.bat
@@ -7,21 +7,27 @@
 :: Kill postgres.exe that is being run by SYSTEM. That is the cause of FME Server failing on first boot.
 :: https://technet.microsoft.com/en-us/library/bb491009.aspx
 :: Restart FME Server Database, because it doesn't start properly 1 time in 5 when first booting
-taskkill /f /t /fi "USERNAME eq SYSTEM" /im postgres.exe > c:\temp\FMEServerRestart.log
-net stop "FME Server Database" >> c:\temp\FMEServerRestart.log
-net start "FME Server Database" >> c:\temp\FMEServerRestart.log
+
+set LOG=c:\temp\OnstartConfiguration.log
+set SSD=z:\
+
+taskkill /f /t /fi "USERNAME eq SYSTEM" /im postgres.exe > %LOG%
+net stop "FME Server Database" >> %LOG%
+net start "FME Server Database" >> %LOG%
 
 ::This is a test of GitHUb
 ::test
 
 :: Ken's Email Configuration
 :: Remember to handle the FMW file.
-net stop SMTPRelay >> c:\temp\FMEServerRestart.log
-C:\apps\FME\fme.exe "C:\Users\Administrator\Documents\My FME Workspaces\SMTPConfigure.fmw" >> c:\temp\FMEServerRestart.log
-copy C:\apps\FMEServer\Utilities\smtprelay\james\apps\james\SAR-INF\config_fme.xml C:\apps\FMEServer\Utilities\smtprelay\james\apps\james\SAR-INF\config.xml /Y >> c:\temp\FMEServerRestart.log
-net start SMTPRelay >> c:\temp\FMEServerRestart.log
+net stop SMTPRelay >> %LOG%
+C:\apps\FME\fme.exe "C:\Users\Administrator\Documents\My FME Workspaces\SMTPConfigure.fmw" >> %LOG%
+copy C:\apps\FMEServer\Utilities\smtprelay\james\apps\james\SAR-INF\config_fme.xml C:\apps\FMEServer\Utilities\smtprelay\james\apps\james\SAR-INF\config.xml /Y >> %LOG%
+net start SMTPRelay >> %LOG%
 
-:: New Comment test
-:: Another new comment
-:: 3rd comment test
-:: another test
+::Copy FMEDATA onto the SSD drive for better performance, or backup.
+for /f "delims=" %%a in ('dir /b/ad "c:\fmedata*" ') robocopy c:\%%a %SSD%\%%a /E >> %LOG%
+
+::Warn people not to put permanent stuff on the SSD drive.
+echo "This is a temporary drive. It is deleted upon shutdown. Use with caution" > "%SSD%\This is a temporary drive.txt"
+
