e3d1071fe9a8e3c6a0960e2ae7eb83892fec4cfb
Ryan Cragg <rjcragg@gmail.com>
2016-07-18 19:49:49 -0700

    Add Esri course resource link

--- OnstartConfiguration.bat
+++ OnstartConfiguration.bat
@@ -50,6 +50,11 @@ echo URL=https://knowledge.safe.com/content/kbentry/25216/fme-desktop-basic-trai
 echo [InternetShortcut] > "c:\users\public\desktop\FME Server Course Resources.url"
 echo URL=https://knowledge.safe.com/content/kbentry/28253/fme-server-authoring-training-course-resources.html >> "c:\users\public\desktop\FME Server Course Resources.url"
 
+
+:: FME Server Course Resources
+echo [InternetShortcut] > "c:\users\public\desktop\FME Esri Course Resources.url"
+echo URL=https://knowledge.safe.com/articles/30330/fme-desktop-for-esri-training-course-resources.html >> "c:\users\public\desktop\FME Esri Course Resources.url"
+
 :: Your Computer DNS Name
 echo [InternetShortcut] > "c:\users\public\desktop\Your Computer DNS Name.url"
 echo URL=http://169.254.169.254/latest/meta-data/public-hostname >>"c:\users\public\desktop\Your Computer DNS Name.url"
