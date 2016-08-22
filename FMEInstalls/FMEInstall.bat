bba387b5a5ab6fd50f26dc7cd7e78be2c3587512
Ryan Cragg <rjcragg@gmail.com>
2016-08-22 16:33:38 -0700

    Make it smarter

--- FMEInstalls/FMEInstall.bat
+++ FMEInstalls/FMEInstall.bat
@@ -1,17 +1,26 @@
-::Turn of echo and get into C:\temp
 @echo off
 pushd c:\temp
 
-::Prompt for year and build number
+:check
+IF %1.==. GOTO ask
+IF %2.==. GOTO ask
+
+set FMEYEAR=%1
+set FMEBUILD=%2
+goto work
+
+:ask
 set /P FMEYEAR=Enter Year of FME Release:
 set /P FMEBUILD=Enter FME Build Number:
+goto work
 
-::Download all the installers
+:work
 aria2c http://downloads.safe.com/fme/%FMEYEAR%/fme-desktop-b%FMEBUILD%-win-x86.msi --allow-overwrite=true
 aria2c http://downloads.safe.com/fme/%FMEYEAR%/fme-desktop-b%FMEBUILD%-win-x64.msi --allow-overwrite=true
 aria2c http://downloads.safe.com/fme/%FMEYEAR%/fme-server-b%FMEBUILD%-win-x86.msi --allow-overwrite=true
 
-::Run all the installers
 msiexec /i fme-desktop-b%FMEBUILD%-win-x86.msi /qb INSTALLLEVEL=3 INSTALLDIR="c:\apps\FME" ENABLE_POST_INSTALL_TASKS=no
 msiexec /i fme-desktop-b%FMEBUILD%-win-x64.msi /qb INSTALLLEVEL=3 INSTALLDIR="c:\Program Files\FME" ENABLE_POST_INSTALL_TASKS=no
 msiexec /i fme-server-b%FMEBUILD%-win-x86.msi /qb /norestart /l*v installFMEServerLog.txt FMESERVERHOSTNAME=localhost
+
+exit /b
