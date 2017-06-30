dea56a15343082686a56809d6236692ed55bfae4
Ryan Cragg <rjcragg@gmail.com>
2017-06-30 12:25:08 -0700

    Update FMEInstall.bat

--- FMEInstalls/FMEInstall.bat
+++ FMEInstalls/FMEInstall.bat
@@ -10,12 +10,12 @@ set FMEBUILD=%2
 goto work
 
 :ask
-set /P FMEYEAR=Enter FME Release (ie. 2017.1):
+set /P FMERELEASE=Enter FME Release (ie. 2017.1):
 set /P FMEBUILD=Enter FME Build Number:
 goto work
 
 :work
-set FMEYEAR=%FMEBUILD:~0,4%
+set FMEYEAR=%FMERELEASE:~0,4%
 aria2c http://downloads.safe.com/fme/%FMEYEAR%/fme-desktop-%FMERELEASE%-b%FMEBUILD%-win-x86.msi --allow-overwrite=true
 aria2c http://downloads.safe.com/fme/%FMEYEAR%/fme-desktop-%FMERELEASE%-b%FMEBUILD%-win-x64.msi --allow-overwrite=true
 aria2c http://downloads.safe.com/fme/%FMEYEAR%/fme-server-%FMERELEASE%-b%FMEBUILD%-win-x86.msi --allow-overwrite=true
