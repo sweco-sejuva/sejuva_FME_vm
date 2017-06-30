3b1f0a8fdcdfc2d0cbf53e72e97b8d37729b07bf
Ryan Cragg <rjcragg@gmail.com>
2017-06-30 11:37:32 -0700

    Update paths

--- FMEInstalls/FMEInstall.bat
+++ FMEInstalls/FMEInstall.bat
@@ -5,22 +5,23 @@ pushd c:\temp
 IF %1.==. GOTO ask
 IF %2.==. GOTO ask
 
-set FMEYEAR=%1
+set FMERELEASE=%1
 set FMEBUILD=%2
 goto work
 
 :ask
-set /P FMEYEAR=Enter Year of FME Release:
+set /P FMEYEAR=Enter FME Release (ie. 2017.1):
 set /P FMEBUILD=Enter FME Build Number:
 goto work
 
 :work
-aria2c http://downloads.safe.com/fme/%FMEYEAR%/fme-desktop-b%FMEBUILD%-win-x86.msi --allow-overwrite=true
-aria2c http://downloads.safe.com/fme/%FMEYEAR%/fme-desktop-b%FMEBUILD%-win-x64.msi --allow-overwrite=true
-aria2c http://downloads.safe.com/fme/%FMEYEAR%/fme-server-b%FMEBUILD%-win-x86.msi --allow-overwrite=true
+set FMEYEAR=%FMEBUILD:~0,4%
+aria2c http://downloads.safe.com/fme/%FMEYEAR%/fme-desktop-%FMERELEASE%-b%FMEBUILD%-win-x86.msi --allow-overwrite=true
+aria2c http://downloads.safe.com/fme/%FMEYEAR%/fme-desktop-%FMERELEASE%-b%FMEBUILD%-win-x64.msi --allow-overwrite=true
+aria2c http://downloads.safe.com/fme/%FMEYEAR%/fme-server-%FMERELEASE%-b%FMEBUILD%-win-x86.msi --allow-overwrite=true
 
-msiexec /i fme-desktop-b%FMEBUILD%-win-x86.msi /qb INSTALLLEVEL=3 INSTALLDIR="c:\apps\FME" ENABLE_POST_INSTALL_TASKS=no
-msiexec /i fme-desktop-b%FMEBUILD%-win-x64.msi /qb INSTALLLEVEL=3 INSTALLDIR="c:\Program Files\FME" ENABLE_POST_INSTALL_TASKS=no
-msiexec /i fme-server-b%FMEBUILD%-win-x86.msi /qb /norestart /l*v installFMEServerLog.txt FMESERVERHOSTNAME=localhost
+msiexec /i fme-desktop-%FMERELEASE%-b%FMEBUILD%-win-x86.msi /qb INSTALLLEVEL=3 INSTALLDIR="c:\apps\FME" ENABLE_POST_INSTALL_TASKS=no
+msiexec /i fme-desktop-%FMERELEASE%-b%FMEBUILD%-win-x64.msi /qb INSTALLLEVEL=3 INSTALLDIR="c:\Program Files\FME" ENABLE_POST_INSTALL_TASKS=no
+msiexec /i fme-server-%FMERELEASE%-b%FMEBUILD%-win-x86.msi /qb /norestart /l*v installFMEServerLog.txt FMESERVERHOSTNAME=localhost
 
 exit /b
