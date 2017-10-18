f793add73d7af0bd1bb319329cb07d41ddc1e3a6
Ryan Cragg <rjcragg@gmail.com>
2017-10-18 15:09:23 -0700

    Update for new machine

--- CleanMachine.bat
+++ CleanMachine.bat
@@ -1,12 +1,11 @@
-::This file does the initial configuration of the AWS instance; things that you only want to do once, like name the computer.
-::Assuming that a T2.large is being used. Provide at least 70GB of storage.
+::This file does the initial configuration of the AWS instance; things that you only want to do once, like name the computer and set the password.
 ::Download and run this from the (elevated?) command line (Win+R, CMD) by using the following command:
 :: powershell -Command "Invoke-WebRequest https://raw.githubusercontent.com/rjcragg/AWS/2016/CleanMachine.bat -OutFile CleanMachine.bat" && CleanMachine.bat
 ::OR use User Data when creating the EC2 instance. Past in the following script:
-:: <script>powershell -Command "Invoke-WebRequest https://raw.githubusercontent.com/rjcragg/AWS/master/CleanMachine.bat -OutFile CleanMachine.bat" && CleanMachine.bat</script>
+:: <script>powershell -Command "Invoke-WebRequest https://raw.githubusercontent.com/rjcragg/AWS/master/CleanMachine.bat -OutFile CleanMachine.bat" && CleanMachine.bat password</script>
 :main
 	::::GENERAL SETTINGS FOR LATER IN BATCH FILE, and run procedures::::
-		set EC2PASSWORD=FME2016learnings
+		set EC2PASSWORD=%1
 		set LOG=c:\temp\InitialConfiguration.log
 		set TEMP=c:\temp
 	::Make required folders
@@ -35,7 +34,7 @@ goto :eof
 :ec2Setup
 	::::CONFIGURE WINDOWS SETTINGS::::
 	:: Set the time zone
-	::	tzutil /s "Pacific Standard Time"
+		tzutil /s "Pacific Standard Time"
 	:: The purpose of this section is to configure proxy ports for Remote Desktop
 	:: It must be run with elevated permissions (right-click and run as administrator)
 	:: The batch file assumes the computer name will not change.
@@ -52,20 +51,8 @@ goto :eof
 	::Install Chocolatey  https://chocolatey.org/
 		@powershell -NoProfile -ExecutionPolicy unrestricted -Command "(iex ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1'))) >$null 2>&1" && SET PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin
 	:: Chocolatey allows you to specify what you want on a single line. Let's try that
-	::Bitsadmin does not work in a scheduled task. Install aria2. It is amazingly fast.
-	::We'll need to unzip stuff eventually so get devbox-unzip
-	::I'm sure GIT will be useful at some point
-	::WinDirStat is useful for finding what is taking up drive space.
-	::UltraVNC is useful when helping students troubleshoot. Why 2 passwords? The first is for full control; the second is for view-only.
-	::Google Chrome and Firefox are useful web browsers
-	::Adobe Reader is used to read manuals
-	::Notepad++ is great for text editing
-	::Google Earth is useful
-	::Install Python and Eclipse
-		choco install notepadplusplus google-chrome-x64 firefox adobereader -y
-	::Create a scheduled task to start VNCServer. If it is a service, you have to log in, and that kicks out the student
-	::	"C:\Program Files\uvnc bvba\UltraVNC\setpasswd.exe" safevnc safevnc2 
-	::	schtasks /Create /F /TN UltraVNCServer /SC ONLOGON /TR "C:\Program Files\uvnc bvba\UltraVNC\winvnc.exe"
+		choco install notepadplusplus googlechrome -y
+	
 goto :eof
 
 
