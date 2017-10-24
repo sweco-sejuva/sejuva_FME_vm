cf586386f47fde6600afd20446faa1f595144300
Ryan Cragg <rjcragg@gmail.com>
2017-10-24 14:22:03 -0700

    Update OnLogonConfiguration.bat

--- OnLogonConfiguration.bat
+++ OnLogonConfiguration.bat
@@ -8,6 +8,7 @@ call :VNC > %LOG%
 exit /b
 
 :VNC
+::If the logon is via Remote Desktop, kill winvnc.exe, stop the service, and then start winvnc as the logged in user.
   IF NOT %SESSIONNAME%==Console (
     taskkill /f /t /fi "USERNAME eq SYSTEM" /im winvnc.exe
     net stop "uvnc_service"
