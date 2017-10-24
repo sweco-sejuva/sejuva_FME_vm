00c4a730b87506e537ef9b94243542e191a6a6d3
Ryan Cragg <rjcragg@gmail.com>
2017-10-24 13:18:47 -0700

    configure logging

--- OnLogonConfiguration.bat
+++ OnLogonConfiguration.bat
@@ -1,6 +1,13 @@
 ::On Logon Configuration
 ::These are things that should only happen once a user has logged in.
 
+::Set all required variables
+  set LOG=c:\temp\OnstartLogon.log
+
+call :VNC > %LOG%
+exit /b
+
+:VNC
   IF NOT %SESSIONNAME%==Console (
     taskkill /f /t /fi "USERNAME eq SYSTEM" /im winvnc.exe
     net stop "uvnc_service"
