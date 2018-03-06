77e7eadb126b95c99b87fa2c17ccab4c0ac19a16
Ryan Cragg <rjcragg@gmail.com>
2018-03-06 10:55:36 -0800

    Remove IF
    %SESSIONNAME% is not set until AFTER logon, so it won't work in this situation.

--- OnLogonConfiguration.bat
+++ OnLogonConfiguration.bat
@@ -10,10 +10,10 @@ exit /b
 :VNC
 ::If the logon is via Remote Desktop, kill winvnc.exe, stop the service, and then start winvnc as the logged in user.
 echo %SESSIONNAME%
-  IF NOT %SESSIONNAME%==Console (
+::  IF NOT %SESSIONNAME%==Console (
     taskkill /f /t /fi "USERNAME eq SYSTEM" /im winvnc.exe
     net stop "uvnc_service"
     "C:\Program Files\uvnc bvba\UltraVNC\setpasswd.exe" safevnc safevnc2 
     start "" "C:\Program Files\uvnc bvba\UltraVNC\winvnc.exe"
-  )
+ :: )
 goto :eof
