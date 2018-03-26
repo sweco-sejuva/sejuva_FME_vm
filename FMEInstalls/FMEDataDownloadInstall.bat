b9fa25080f1fa69214159502c5a9e23d9950c991
Ryan Cragg <rjcragg@gmail.com>
2018-03-26 16:30:19 -0700

    Update FMEDataDownloadInstall.bat

--- FMEInstalls/FMEDataDownloadInstall.bat
+++ FMEInstalls/FMEDataDownloadInstall.bat
@@ -1,4 +1,10 @@
-	
-  aria2c https://bluesky-safe-software.fmecloud.com/fmedatastreaming/FMETraining/CurrentFMEDataDownloadURL.fmw --out=CurrentFMEDataDownloadURL.txt --allow-overwrite=true
-	aria2c -i CurrentFMEDataDownloadURL.txt --allow-overwrite=true
-	for %%f in (FMEDATA*.zip) do 7z x -oc:\ -aoa %%f
+:: Downloads and unzips the current FMEDataXXXX from www.safe.com/fmedata
+
+::Obtain link to FMEData
+aria2c https://bluesky-safe-software.fmecloud.com/fmedatastreaming/FMETraining/CurrentFMEDataDownloadURL.fmw --out=CurrentFMEDataDownloadURL.txt --allow-overwrite=true
+
+:Download FMEData
+aria2c -i CurrentFMEDataDownloadURL.txt --allow-overwrite=true
+
+:Unzip FMEData
+for %%f in (FMEDATA*.zip) do 7z x -oc:\ -aoa %%f
