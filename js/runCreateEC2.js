b743fcf4401328ad7fe74086ad8e5722115e60d9
Ryan Cragg <rjcragg@gmail.com>
2018-09-21 13:05:11 -0700

    Update runCreateEC2.js

--- js/runCreateEC2.js
+++ js/runCreateEC2.js
@@ -10,8 +10,8 @@ $(document).ready(function() {
 var dataDist = (function () {
 
   // privates
-  var repository = init.repository; //'FMETraining';
-  var workspaceName = init.workspaceName; //'PythonVMCreator.fmw';
+  var repository = 'FMETraining';
+  var workspaceName = 'PythonVMCreator.fmw';
   var host ;
   var token ;
 
