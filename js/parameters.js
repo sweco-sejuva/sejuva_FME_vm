2f05ed39c3b957ae459e53066a8ea6eff8e04e7b
Ryan Cragg <rjcragg@gmail.com>
2018-10-24 15:24:27 -0700

    Create parameters.js

---
+++ js/parameters.js
@@ -0,0 +1,5 @@
+var repository, workspace, form, server, token;
+repository = "FMETraining";
+workspace = "PythonVMCreator.fmw";
+server = "https://bluesky-safe-software.fmecloud.com";
+token ="0da8284a549462da314f97b336723e23178655d5"; 
