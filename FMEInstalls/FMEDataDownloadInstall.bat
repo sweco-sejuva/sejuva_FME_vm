2bd9fa4a0485a4b1c81084b56fcb8b0e50ee3650
Ryan Cragg <rjcragg@gmail.com>
2018-11-29 09:47:18 -0800

    Fix aria2c issue with file reading

--- FMEInstalls/FMEDataDownloadInstall.bat
+++ FMEInstalls/FMEDataDownloadInstall.bat
@@ -4,9 +4,10 @@
 aria2c https://bluesky-safe-software.fmecloud.com/fmedatastreaming/FMETraining/CurrentFMEDataDownloadURL.fmw --out=CurrentFMEDataDownloadURL.txt --allow-overwrite=true
 
 :Download FMEData
-aria2c -i CurrentFMEDataDownloadURL.txt --allow-overwrite=true
-aria2c https://s3.amazonaws.com/FMEData/GeoAlbertaWorkshops.zip --out=GeoAlbertaWorkshops.zip --allow-overwrite=true
+set /p CurrentFMEDataDownloadURL=<CurrentFMEDataDownloadURL.txt
+aria2c %CurrentFMEDataDownloadURL% --allow-overwrite=true
+::aria2c https://s3.amazonaws.com/FMEData/GeoAlbertaWorkshops.zip --out=GeoAlbertaWorkshops.zip --allow-overwrite=true
 
 :Unzip FMEData
 for %%f in (FMEDATA*.zip) do 7z x -oc:\ -aoa %%f
-7z x -oc:\FMEData2018\Resources\GeoAlbertaWorkshops\ -aoa GeoAlbertaWorkshops.zip
+::7z x -oc:\FMEData2018\Resources\GeoAlbertaWorkshops\ -aoa GeoAlbertaWorkshops.zip
