8a5a9dda0820aa09f43d38b4b7e6a78d9b4d64b5
Ryan Cragg <rjcragg@gmail.com>
2019-01-14 18:21:22 -0800

    Update parameters.js

--- js/parameters.js
+++ js/parameters.js
@@ -1,6 +1,6 @@
 var repository, workspace, form, server, token;
 repository = "FMETraining";
-workspace = "PythonVMCreator.fmw";
+workspace = "VMCreator.fmw";
 server = "https://demos-safe-software.fmecloud.com";
 token ="b42f5405ae92f30024974cd3943d5d2a2bcf0443"; 
 
