e9e020d96e479cb848108fcf93acc482c0896aa8
Ryan Cragg <rjcragg@gmail.com>
2019-01-14 22:11:28 -0800

    Update parameters.js

--- js/parameters.js
+++ js/parameters.js
@@ -1,13 +1,13 @@
 var repository, workspace, form, server, token;
-repository = "FMETraining";
-workspace = "VMCreator.fmw";
-server = "https://demos-safe-software.fmecloud.com";
-token ="b42f5405ae92f30024974cd3943d5d2a2bcf0443"; 
+repository = "FMETraining";  //This is the FME Server repository
+workspace = "VMCreator.fmw"; //This is the workspace that creates the virtual machines
+server = "https://demos-safe-software.fmecloud.com";  //This is the URL of your FME Server or FME Cloud
+token ="b42f5405ae92f30024974cd3943d5d2a2bcf0443";     //security token for the user that runs the workspace.
 
 var region_name = "us-east-1";
 var EC2Type = "t3";
 var EC2Size = "large";
-var CourseType = "training";
+var CourseType = "training";   //matches the tag on the AMI
 var AccountEmail = "train@safe.com";
 var EmergencyEmail = "ryan.cragg@safe.com";
 var CRMEmail = "emailtosalesforce@3bxfu520n23ghfjugzh0uta0y.in.salesforce.com";
