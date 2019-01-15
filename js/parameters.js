c2c794b326fe94805fa22cbb6027ced70eccf665
Ryan Cragg <rjcragg@gmail.com>
2019-01-14 18:20:08 -0800

    Update parameters.js

--- js/parameters.js
+++ js/parameters.js
@@ -3,3 +3,11 @@ repository = "FMETraining";
 workspace = "PythonVMCreator.fmw";
 server = "https://demos-safe-software.fmecloud.com";
 token ="b42f5405ae92f30024974cd3943d5d2a2bcf0443"; 
+
+var region_name = "us-east-1";
+var EC2Type = "t3";
+var EC2Size = "large";
+var CourseType = "training";
+var AccountEmail = "train@safe.com";
+var EmergencyEmail = "ryan.cragg@safe.com;
+var CRMEmail = "emailtosalesforce@3bxfu520n23ghfjugzh0uta0y.in.salesforce.com";
