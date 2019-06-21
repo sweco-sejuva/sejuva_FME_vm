87f7f3291ed3df84a13fea4da124c2b975c44d59
Ryan Cragg <ryan.cragg@safe.com>
2019-06-21 14:09:41 -0700

    Create RAWGIT enviroment variable. Fix FME Server installation

--- FMEInstalls/FMEDownloadInstall.bat
+++ FMEInstalls/FMEDownloadInstall.bat
@@ -5,16 +5,16 @@ pushd c:\temp
 
 :: clean up existing installers
 del fme-desktop*.msi
-del fme-server*.msi
+del fme-server*.exe
 
 ::download new installers
-aria2c https://bluesky-safe-software.fmecloud.com/fmedatastreaming/FMETraining/DownloadList.fmw --out=FMEs.txt --allow-overwrite=true 
+aria2c https://bluesky-safe-software.fmecloud.com/fmedatastreaming/FMETraining/DownloadList.fmw --out=FMEs.txt --allow-overwrite=true
 aria2c -i FMEs.txt --allow-overwrite=true
 
 ::install FME Desktop and Server
 for %%f in (fme-desktop*.msi) do msiexec /i %%f /qb INSTALLLEVEL=3 INSTALLDIR="c:\Program Files\FME" ENABLE_POST_INSTALL_TASKS=no
-for %%f in (fme-server*.msi) do msiexec /i %%f /qb /norestart /l*v installFMEServerLog.txt FMESERVERHOSTNAME=localhost
+for %%f in (fme-server*.exe) do %%f -d "c:\program files\FMEServer" -s -sp"/qb /norestart /l*v installFMEServerLog.txt FMESERVERHOSTNAME=localhost"
 
 ::License FME Desktop and Server. These should be existing environment variables.
-"c:\Program Files\fme\fmelicensingassistant_cmd.exe" --floating %FMELICENSEIP% smallworld	
-"c:\Program Files\fmeserver\server\fme\fmelicensingassistant_cmd.exe" --serial %FMESERVERSERIAL%
+"c:\Program Files\fme\fmelicensingassistant_cmd.exe" --floating %FMELICENSEIP% smallworld
+::"c:\Program Files\fmeserver\server\fme\fmelicensingassistant_cmd.exe" --serial %FMESERVERSERIAL%
--- PostCreationSteps.md
+++ PostCreationSteps.md
@@ -3,4 +3,4 @@
 3. Open FME Workbench and Data Inspector to ensure they are properly licensed, and pin them to Task Bar.
 4. Open Localhost in browser, and ensure that FME Server is properly licensed.
 6. Open Google Earth to see if it has a DirectX vs OpenGL warning. Set appropriately.
-5. Create new AMI.
+5. Create new AMI. Set the Description as the Branch.
